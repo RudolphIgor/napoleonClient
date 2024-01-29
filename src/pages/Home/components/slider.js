625c14ac96c1d6a414da7ab24cb43b0799131863
Igor Rudolf <azazil47@gmail.com>
2024-01-29 15:40:54 +0700

    Втроен слайдер, но пока не работает1

--- src/pages/Home/components/slider.js
+++ src/pages/Home/components/slider.js
@@ -10,7 +10,7 @@ import 'swiper/css';
 
 export default () => {
     return (
-        <Swiper
+        <Swiper className='swiper'
             // install Swiper modules
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={50}
@@ -21,7 +21,7 @@ export default () => {
             onSwiper={(swiper) => console.log(swiper)}
             onSlideChange={() => console.log('slide change')}
         >
-            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide >Slide 1</SwiperSlide>
             <SwiperSlide>Slide 2</SwiperSlide>
             <SwiperSlide>Slide 3</SwiperSlide>
             <SwiperSlide>Slide 4</SwiperSlide>
