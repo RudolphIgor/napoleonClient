5cb005b8ca1d3a2d4d20c7753c764441883a6aed
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-01-31 14:12:06 +0700

    Добавлен и работает слайдер. Но еще не стилизован

--- src/components/Section1/index.js
+++ src/components/Section1/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {ReactComponent as Logo} from './../../img/logo.svg'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 const Index = () => {
     return (
-        <div>
-            Будущий слайдер
-        </div>
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log('slide change')}
+        >
+            <SwiperSlide>
+                <Logo/>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Logo/>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Logo/>
+            </SwiperSlide>
+
+
+        </Swiper>
     );
 };
 
