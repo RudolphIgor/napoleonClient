57816f3919e73f78a513b76dbf2b450730a54ae5
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-01-31 14:00:15 +0700

    Добавлен вывод секции 1 на домашнюю страницу

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './pages.module.css'
+import Section1 from "../components/Section1";
 
 const Auth = () => {
     return (
         <div className={style.container}>
-            HOME
+            <Section1/>
 
         </div>
     );
