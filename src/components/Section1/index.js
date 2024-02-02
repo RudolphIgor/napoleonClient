dec976f6ed0fee85ffe315e6e3de16730c85853f
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-02-02 15:04:02 +0700

    Реализована автопрокрутка слайдов

--- src/components/Section1/index.js
+++ src/components/Section1/index.js
@@ -1,39 +1,44 @@
 import React from 'react';
 import style from './section1.module.css'
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Autoplay, EffectFade} from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+// import {Autoplay} from "swiper/modules";
 
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+
+import 'swiper/css/effect-fade';
 import Slider from "./Slider";
+import 'swiper/css/autoplay'
 const Index = () => {
     return (
         <div className={style.wrapper}>
-            <Slider/>
-            {/*<Swiper*/}
-            {/*    modules={[Navigation, Pagination, Scrollbar, A11y]}*/}
-            {/*    spaceBetween={50}*/}
-            {/*    slidesPerView={1}*/}
-            {/*    navigation*/}
-            {/*    pagination={{ clickable: true }}*/}
-            {/*    scrollbar={{ draggable: true }}*/}
-            {/*    onSwiper={(swiper) => console.log(swiper)}*/}
-            {/*    onSlideChange={() => console.log('slide change')}*/}
-            {/*>*/}
-            {/*    <SwiperSlide>*/}
-            {/*        <Logo/>*/}
-            {/*    </SwiperSlide>*/}
-            {/*    <SwiperSlide>*/}
-            {/*        <Logo/>*/}
-            {/*    </SwiperSlide>*/}
-            {/*    <SwiperSlide>*/}
-            {/*        <Logo/>*/}
-            {/*    </SwiperSlide>*/}
+
+            <Swiper
+                modules={[Autoplay, EffectFade]}
+                effect="fade"
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false
+                }}
+                spaceBetween={50}
+                slidesPerView={1}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+                <SwiperSlide>
+                    <Slider/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slider/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slider/>
+                </SwiperSlide>
 
 
-            {/*</Swiper>*/}
+            </Swiper>
         </div>
 
     );
