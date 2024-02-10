6ff1ac71a06260660966c5680f66742e40ea7d1e
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-02-10 16:58:55 +0700

    Реализован слайдер

--- src/components/Section1/Slider/index.js
+++ src/components/Section1/Slider/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import style from './slider.module.css'
 import mainStyle from '../../../styles/index.module.css'
-import Image from '../../../img/slider/1.jpg'
 
-
+const idImg = 'http://178.208.66.209:3000/api/images/get/'
 const Index = (props) => {
     const styleblock = {
-        background: `url(${Image}) center no-repeat`,
+        // background: `url(${Image}) center no-repeat`,
+        background: `url(${idImg}${props.slideArray.imageid}) center no-repeat`,
         height: '498px',
     }
 
     return (
-        <div  style={styleblock} className={style.wrapper}>
-                {/*<p className={style.title}>*/}
-
-                {/*</p>*/}
+        <div style={styleblock} className={style.wrapper}>
             <div className={`${style.content} ${mainStyle.container}`}>
                 <p className={style.title}>
                     {props.slideArray.text}
@@ -24,7 +21,6 @@ const Index = (props) => {
                 </button>
             </div>
         </div>
-
     );
 };
 
